3157d774dc00f14ddd678ca5f9fb7795e2dfc75f
Teresa Orozco Bon <51003509+TereBonMTWDM@users.noreply.github.com>
2024-12-04 16:28:48 -0600

    DELETE other data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@
 export const environment = {
   production: false,
   api: {
-    Cobranza: 'https://localhost:7237/api/',
     Inventario: 'https://localhost:7105/api/'
   },
   firebaseConfig: {
@@ -18,13 +17,6 @@ export const environment = {
     appId: "1:1:1035440965014:web:b0410a07cc0d08f26489b7",
     measurementId: "G-R3NRBH7C3Z"
   }
-  // firebaseConfig: {
-  //   apiKey: "AIzaSyABWZtYxhPZZiAiqXUMTqU4ZHGDWfD116g",
-  //   authDomain: "inverser-front.firebaseapp.com",
-  //   projectId: "inverser-front",
-  //   storageBucket: "inverser-front.appspot.com",
-  //   messagingSenderId: "879285539504",
-  //   appId: "1:879285539504:web:ed92f45942b5da3fa3a175"
-  // }
+ 
 
 };
